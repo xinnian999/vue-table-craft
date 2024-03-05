88227cc3916999da436194a4b26fc50baa3c51e9
hyl <3307578337@qq.com>
2024-03-05 15:05:24 +0800

    引入el

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <SchemaTable />
 </template>
+
+<script setup lang="ts">
+import SchemaTable from '@/components/SchemaTable.vue'
+</script>
